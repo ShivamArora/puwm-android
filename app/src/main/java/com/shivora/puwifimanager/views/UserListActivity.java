e578c63f905e08e40c68aa2748675dc45dbd9cd8
Shivam Arora <email.shivamarora@gmail.com>
2018-08-17 19:52:27 +0530

    Fix anydpi icon and add rate us option

--- app/src/main/java/com/shivora/puwifimanager/views/UserListActivity.java
+++ app/src/main/java/com/shivora/puwifimanager/views/UserListActivity.java
@@ -5,10 +5,12 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -209,6 +211,20 @@ public class UserListActivity extends AppCompatActivity implements ListItemClick
                         FlashbarUtils.showMessageDialog((Activity) context,"Logout Sucessful!", "User has been logged out successfully!");
                     }
                 });
+                return true;
+            case R.id.action_rate_us:
+                Uri uri = Uri.parse("market://details?id="+context.getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW,uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY|Intent.FLAG_ACTIVITY_NEW_DOCUMENT|Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+                try{
+                    startActivity(goToMarket);
+                }
+                catch (ActivityNotFoundException e){
+                    startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://play.google.com/store/apps/details?id="+context.getPackageName())));
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
